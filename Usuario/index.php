622402da7b595c1353c32ba5f6deca0ae0c85a19
dayana rincon <dayana_diaz@misena.edu.co>
2016-05-20 11:21:32 -0500

    utilizando css en la index

--- Usuario/index.php
+++ Usuario/index.php
@@ -10,6 +10,7 @@
 	
 	<meta name="keywords" content="enfermedades,medicamentos,especialistas"/>
 	<link rel="stylesheet" type="text/css" href="css/style.css">
+
 	<title>Inico-Consultas medicas</title>
 
 </head>
@@ -26,70 +27,34 @@
 
 	<nav>	<!--menu-->
 
-		<ul>
-			<a href="index.php"><li>Inicio</li></a>
-			<a href="galeria.php"><li>Galeria</li></a>
-			<a href="especialistas.php"><li>Especialistas</li></a>
-			<a href="quienes_somos.php"><li>Quienes somos</li></a>
+		<ul id="menu">
+			<a href="index.php"><li class="i_menu">Inicio    </li></a>
+			<a href="galeria.php"><li class="i_menu">Galeria   </li></a>
+			<a href="especialistas.php"><li class="i_menu">Especialistas   </li></a>
+			<a href="quienes_somos.php"><li class="i_menu">Quienes somos    </li></a>
 		</ul>	
 
 	</nav>
 	<section><!--informacion principal-->
 
-
-		<h2>El cuidado del cuerpo humano</h2>
-
-		<article>
-
-			<p>Si existe un aspecto de nuestra vida donde no se puede dar el lujo de correr riesgos, 
-			en precisamente en el cuidado de la salud. Es por esto que a través el siguiente articulo 
-			encontraras consejo y las formas mas recomendadas para cuidar tu salud. </p>
-
-			<p>Toma en cuenta que el mismo requiere un cuidado en las seis dimensiones presentadas en el artículo anterior. 
-				Los aspectos más importantes que debes tomar en consideración para cuidar la salud son, 
-				la alimentación, la higiene, la seguridad personal, el cuidado y prevención de enfermedades físicas y psicológicas 
-				y las buenas relaciones interpersonales.</p>
-
-		</article>
+	<h1>Informacion Principal</h1>
 
 		
-
-
-			<ol>
-				<li>Comer equilibradamente</li>
-				<li>Evitar las grasas</li>
-				<li>No abusar de las grasas saturadas</li>
-				<li>No abusar de los alimentos azucarados</li>
-				<li>Privarse de la bollería industrial</li>
-				<li>Realizar ejercicio 3 ó 4 días semanales</li>
-				<li>No excederse en ejercicios cardiovasculares si no se tiene el nivel adecuado</li>
-				<li>Estar bien hidratado. Beber el agua suficiente</li>
-				<li>Dormir no menos de 7 horas diarias</li>
-				<li>Ser positivo y alegre</li>
-
-			</ol>
-
-		<h2>¿Por que es tan importante la salud?</h2>
-
-		<article>Existen varios beneficios de tener una vida saludable, pero el principal de ellos que podríamos nombrar es que nuestro cuerpo 
-			se libera de las diversas formas de trastornos y complicaciones y, por tanto, se obtiene una vida más larga, sin sufrir ningún tipo 
-			de dolores o malestares.Una persona saludable es capaz de rendir al máximo de su capacidad en todos los ámbitos de su vida y
-			con ello se convertirá en una persona valiosa para la sociedad, porque su organismo funciona de manera correcta y puede realizar
-			sus actividades diarias sin ningún problema.</article>
-
-			<img src="img/laSalud.jpg">
-			
-
-
 	</section>
 
 
 	<aside>	<!--barra lateral-->
-		<ul>
-			<a href="enfermedades.php"><li>Enfermedades</li></a>
-			<a href="medicamentos.php"><li>Medicamentos</li></a>
-			<a href="especialistas.php"><li>Especialistas</li></a>
+
+	
+		
+		<ul id="barraLateral">
+			<a href="enfermedades.php"><li class="i_barraLateral">Enfermedades</li></a>
+			<a href="medicamentos.php"><li  class="i_barraLateral">Medicamentos</li></a>
+			<a href="especialistas.php"  class="i_barraLateral"><li>Especialistas</li></a>
 		</ul>
+
+	
+		
 	</aside>
 
 
